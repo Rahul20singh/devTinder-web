7137ddc54acf35a9f16cd739118f847c7ea9f978
Rahul Bisht <rahulbisht575@gmail.com>
2025-07-10 16:08:19 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Body from "./Body";
+import Login from "./Login";
 function App() {
   return (
     <>
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Body />}>
-            <Route path="/login" element={<div>login page</div>} />
+            <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<div>Profile page</div>} />
           </Route>
           <Route path="/login" element={<div>Login Page</div>}></Route>
