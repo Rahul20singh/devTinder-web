420c622c73fb859cc69e618c0249c8cb288c9d99
Rahul Bisht <rahulbisht575@gmail.com>
2025-07-13 00:09:50 +0530

    build the usercard on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import Ucard from "./Ucard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const dispatch = useDispatch();
+  const feed = useSelector((store) => store.feed);
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const users = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      console.log("11111111111", users.data.data);
+      dispatch(addFeed(users.data.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return (
+    feed && (
+      <div>
+        <Ucard user={feed[0]} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,40 +32,42 @@ const NavBar = () => {
           DevTinder
         </Link>
       </div>
-      <div className="flex gap-2">
-        {user && <p>Welcome, {user.firstName} </p>}
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              {user && <img src={user.photoUrl} alt="Profile Photo" />}
+      {user && (
+        <div className="flex gap-2">
+          {user && <p>Welcome, {user.firstName} </p>}
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                {user && <img src={user.photoUrl} alt="Profile Photo" />}
+              </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link
+                  to="/profile"
+                  className="flex justify-between items-center gap-2"
+                >
+                  <span>Profile</span>
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <Link
-                to="/profile"
-                className="flex justify-between items-center gap-2"
-              >
-                <span>Profile</span>
-                <span className="badge">New</span>
-              </Link>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a onClick={handleLogout}>Logout</a>
-            </li>
-          </ul>
         </div>
-      </div>
+      )}
     </div>
   );
 };
